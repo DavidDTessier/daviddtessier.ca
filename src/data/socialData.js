cbb49863ae61bb9c5a11d9f3b1a52cb1cce8f261
DavidDTessier <57716152+DavidDTessier@users.noreply.github.com>
2023-12-22 14:11:52 -0500

    updates

---
+++ src/data/socialData.js
@@ -0,0 +1,31 @@
+const socialData = [
+    {
+        name: "LinkedIn",
+        url: "https://www.linkedin.com/in/david-tessier-66749613/",
+        icon: "fa:linkedin"
+    },
+    {
+        name: "Facebook",
+        url: "https://www.facebook.com/daviddtessier/",
+        icon: "fa-brands:facebook"
+    },
+    {
+        name: "Instagram",
+        url: "https://www.instagram.com/daviddtessier/",
+        icon: "fa-brands:instagram"
+    },
+    {
+        name: "Github",
+        url: "https://github.com/DavidDTessier",
+        icon: "fa-brands:github"
+    },
+    {
+        name: "Reddit",
+        url: "https://www.reddit.com/user/Cl0udS3c_2023",
+        icon: "fa-brands:reddit"
+    },
+
+
+]
+
+export default socialData;
