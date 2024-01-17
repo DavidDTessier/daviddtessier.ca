9a63507eb19a61daa90b7f7e2b73772b731dd79f
DavidDTessier <57716152+DavidDTessier@users.noreply.github.com>
2024-01-17 07:41:00 -0500

    Clean up
    Clean up dependencies and fixes

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,13 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
+import icon from "astro-icon";
+
+import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind({
-    applyBaseStyles: false,
-  }), mdx()],
+    applyBaseStyles: false
+  }), mdx(), icon()]
 });
